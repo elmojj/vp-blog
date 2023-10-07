4c2a34f196935471b86fd9129e618f2becc0ec51
elmoj <jiangxz@info-water.com.cn>
2023-10-07 17:29:51 +0800

    initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // theme: '@vuepress/blog',
+  themeConfig: {
+    // 请参考文档来查看所有可用的选项。
+    logo: './assets/img/logo.jpg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://baidu.com' },
+    ],
+    sidebar: [
+      {
+        title: 'Group 1',   // 必要的
+        path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ /* ... */ ],
+        initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+      }
+    ]
+  }
+}
